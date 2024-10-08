d02296ab8f1fc56b5cc15e03d29c1e25337c1eeb
naz99 <36990555+naz99@users.noreply.github.com>
2024-10-08 20:44:09 +0800

    Update autism_diagnosis_app.py

--- autism_diagnosis_app.py
+++ autism_diagnosis_app.py
@@ -224,7 +224,7 @@ def main():
         intellectual_disability = st.selectbox("Intellectual Disability", options=["Yes", "No"])
         social_behavioral_issues = st.selectbox("Social/Behavioral Issues", options=["Yes", "No"])
         anxiety_disorder = st.selectbox("Anxiety Disorder", options=["Yes", "No"])
-        gender = st.selectbox("Gender", options=["Female, Male"])
+        gender = st.selectbox("Gender", options=["Female", "Male"])
         jaundice = st.selectbox("Suffers from Jaundice", options=["Yes", "No"])
         family_history_asd = st.selectbox("Family History with ASD", options=["Yes", "No"])
 

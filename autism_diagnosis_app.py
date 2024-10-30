37000e5da29d860d755cf7597f6a63585056c288
naz99 <36990555+naz99@users.noreply.github.com>
2024-10-30 14:43:22 +0800

    Update autism_diagnosis_app.py

--- autism_diagnosis_app.py
+++ autism_diagnosis_app.py
@@ -218,7 +218,8 @@ def main():
                 img1 = Image.open("asd_child.jpg")
                 st.image(img1, width=300)
                 
-                 st.markdown("<br><br>", unsafe_allow_html=True)  # Add two line breaks for vertical space
+                st.write("")  # Add two line breaks for vertical space
+                st.write("")
                 
                 img2 = Image.open("autism.png")
                 st.image(img2, width=300)
